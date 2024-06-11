a7052f5739f2dc3d19d48c6cd2133ffec899356f
Vadim Tokar <messiah.turner@gmail.com>
2024-06-11 17:21:09 +0400

    add difficulty levels

--- tests/basics.rs
+++ tests/basics.rs
@@ -22,11 +22,11 @@ fn test() {
         .read_state(pid)
         .expect("Failed to get the initial state of the game");
 
+    assert!(state.pebbles_remaining <= 10);
     assert_eq!(state.pebbles_count, 10);
-    assert_eq!(state.pebbles_remaining, 10);
     assert_eq!(state.max_pebbles_per_turn, 4);
-    // assert_eq!(state.difficulty, DifficultyLevel::Easy);
-    // assert_eq!(state.winner, None::<Player>);
+    assert_eq!(state.difficulty, DifficultyLevel::Easy);
+    assert_eq!(state.winner, None::<Player>);
 
     // Player's (User) turn
     program.send(sender_id, PebblesAction::Turn(2));
@@ -42,17 +42,17 @@ fn test() {
     program.send(sender_id, PebblesAction::GiveUp);
 
     // Check the state after player gives up
-    // let state: GameState = program
-    //     .read_state(pid)
-    //     .expect("Failed to get the state of the game after giving up");
+    let state: GameState = program
+        .read_state(pid)
+        .expect("Failed to get the state of the game after giving up");
 
-    // assert_eq!(state.winner, Some(Player::Program));
+    assert_eq!(state.winner, Some(Player::Program));
 
     // Restart the game
     let restart_message = PebblesAction::Restart {
         difficulty: DifficultyLevel::Hard,
         pebbles_count: 15,
-        max_pebbles_per_turn: 4,
+        max_pebbles_per_turn: 10,
     };
 
     program.send(sender_id, restart_message);
@@ -62,9 +62,9 @@ fn test() {
         .read_state(pid)
         .expect("Failed to get the state of the game after restart");
 
+    assert!(state.pebbles_remaining <= 15);
     assert_eq!(state.pebbles_count, 15);
-    assert_eq!(state.pebbles_remaining, 15);
-    assert_eq!(state.max_pebbles_per_turn, 4);
-    // assert_eq!(state.difficulty, DifficultyLevel::Hard);
-    // assert_eq!(state.winner, None);
+    assert_eq!(state.max_pebbles_per_turn, 10);
+    assert_eq!(state.difficulty, DifficultyLevel::Hard);
+    assert_eq!(state.winner, None);
 }
